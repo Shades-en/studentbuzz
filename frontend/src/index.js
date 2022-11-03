85e6670260576a805978e168106bbaa292c5a67f
Owais Iqbal <owaisiqbal2013@gmail.com>
2022-11-03 23:17:28 +0530

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import * as React from 'react';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: '#69c9d0',
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: '#11cb5f',
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
--- frontend/src/routes/home/home.js
+++ frontend/src/routes/home/home.js
@@ -1,11 +1,23 @@
-const Home = () => {
-  return (  
-      <>
-          <div>
-              <h1>Home</h1>
-          </div>
-      </>
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function MyApp() {
+  return (
+    <>
+      <Button variant="contained">Primary</Button>
+      <Button variant="contained" color="secondary">Secondary</Button>
+    </>
   );
 }
 
-export default Home;
+// const Home = () => {
+//   return (  
+//       <>
+//           <div>
+//               <h1>Home</h1>
+//           </div>
+//       </>
+//   );
+// }
+
+// export default Home;
