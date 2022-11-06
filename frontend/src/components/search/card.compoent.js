c7cff90402faaa0f1b071aab5e49bb3204b63040
Jeevika Kiran <jeevika.kiran@gmail.com>
2022-11-07 01:32:29 +0530

    club clickable cards

--- frontend/src/components/search/card.compoent.js
+++ frontend/src/components/search/card.compoent.js
@@ -1,26 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import "./search.css"
 
 const Cards = () => {
+
+    const clubLink = (goTO) => {
+        <Link to="goTO"></Link>
+    }
+
     return ( 
         <>  
         <div className="clubs_card_container">
-            <div className="card">
+            <div className="card" onClick={() => clubLink()}>
                 <p>Google Developers Student Club</p>
             </div>
-            <div className="card">
+            <div className="card" onClick={() => clubLink()}>
                 <p>CodeRIT</p>
             </div>
-            <div className="card">
+            <div className="card" onClick={() => clubLink()}>
                 <p>Editha</p>
             </div>
-            <div className="card">
+            <div className="card" onClick={() => clubLink()}>
                 <p>EDC</p>
             </div>
-            <div className="card">
+            <div className="card" onClick={() => clubLink()}>
                 <p>Finance Club</p>
             </div>
-            <div className="card">
+            <div className="card" onClick={clubLink}>
                 <p>Velocita</p>
             </div>
         </div>
