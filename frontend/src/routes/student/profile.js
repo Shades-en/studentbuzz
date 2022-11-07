5cca44f96510e21558f2676130a5c49314e3685c
Jeevika Kiran <jeevika.kiran@gmail.com>
2022-11-07 22:24:29 +0530

    some stuff

---
+++ frontend/src/routes/student/profile.js
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+const StudentProfile = () => {
+    return ( 
+        <>
+            <Outlet></Outlet>
+        </>
+     );
+}
+ 
+export default StudentProfile;
