ca99a93a56d06bf405bf93d951228cac06bc6dbb
umraz <kumraz858@gmail.com>
2022-11-11 12:17:44 +0530

    fixed empty space and bg trans

--- frontend/src/components/login/loginFaculty.component.js
+++ frontend/src/components/login/loginFaculty.component.js
@@ -9,8 +9,7 @@ const Login = (props) => {
         <>
             <div className="lg:flex">
                 <div className="mt-7 lg:w-1/2 xl:max-w-screen-sm">
-                    <div className="py-12 bg-indigo-500 lg:bg-white flex justify-center lg:justify-start lg:px-12">
-                    </div>
+                    
                     <div className="mt-10 px-12 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:px-24 xl:max-w-2xl">
 
                         {type.faculty &&  <h3 className="text-left text-4xl  font-display font-bold lg:text-left xl:text-4xl
@@ -22,7 +21,7 @@ const Login = (props) => {
                             <form>
                                 <div>
                                     <div className="text-sm font-bold text-gray-700 tracking-wide">Email Address</div>
-                                    <input className="w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500" type="" placeholder="mike@gmail.com"/>
+                                    <input className="bg-black/0 w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500" type="" placeholder="mike@gmail.com"/>
                                 </div>
                                 <div className="mt-8">
                                     <div className="flex justify-between items-center">
@@ -30,7 +29,7 @@ const Login = (props) => {
                                             Password
                                         </div>
                                     </div>
-                                    <input className="w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500" type="" placeholder="Enter your password"/>
+                                    <input className="bg-black/0 w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500" type="" placeholder="Enter your password"/>
                                 </div>
                                 <div className="mt-10 flex justify-center">
                                     <button className="bg-indigo-500 text-gray-100 p-3 w-10/12 rounded-full tracking-wide
